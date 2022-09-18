c73f3ced3897d47cd728f394c6c8eb1449a7a8c4
MichaelKerr0702 <110789076+MichaelKerr0702@users.noreply.github.com>
2022-09-18 14:24:07 -0700

    added js and HTML code made working program

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,49 @@
-var date = moment().format("dddd, MMMM Do");
-$('#currentDay').text(date);
+$(document).ready(function () {
+    // set time and date 
+    var date = moment().format("dddd, MMMM Do");
+    $('#currentDay').text(date);
+
+    // save to local storage when button is pressed
+    $('.saveBtn').on('click', function (){
+        var key = $(this).parent().attr('id');
+        var value = $(this).siblings('.description').val();
+
+        localStorage.setItem(key, value);        
 
 
-function ChangeColor(){
-    for(var i= 0; i > 14 ; i++){
-        var currentTime = "time"+ i;
-        console.log(currentTime);
-        console.log(i)
+    })
+
+    //change colors of blocks
+    function PastPresentFuture(){
+        //get current hour 
+        var currentHour = moment().format('H');
+
+        //check time compared to color of text block
+        $('.time-block').each(function () {
+            var TimeBlock = parseInt($(this).attr('id').split('-')[1]);
+
+            if (TimeBlock < currentHour){
+                $(this).addClass('past');
+            } else if (TimeBlock === currentHour){
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        });
     }
-}
-ChangeColor();
+
+    PastPresentFuture();
+
+    $('#time-9 .description').val(localStorage.getItem('time-9'));
+    $('#time-10 .description').val(localStorage.getItem('time-10'));
+    $('#time-11 .description').val(localStorage.getItem('time-11'));
+    $('#time-12 .description').val(localStorage.getItem('time-12'));
+    $('#time-13 .description').val(localStorage.getItem('time-13'));
+    $('#time-14 .description').val(localStorage.getItem('time-14'));
+    $('#time-15 .description').val(localStorage.getItem('time-15'));
+    $('#time-16 .description').val(localStorage.getItem('time-16'));
+    $('#time-17 .description').val(localStorage.getItem('time-17'));
+})
